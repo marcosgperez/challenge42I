a7d225336a0c05a0fa4a04e4405457c4ef9c5259
leblurz <marcoss.g.perez@gmail.com>
2022-04-08 12:45:31 -0300

    finish

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import Matriz from "../components/Matriz";
+import RightArrow from "../components/RightArrow";
+import LeftArrow from "../components/LeftArrow";
+import Trash from "../components/Trash";
+
 
 const Home = (props) => {
+  const Tiles = Array(8 * 8)
+    .fill(0)
+    .map((_, i) => ({
+      checked: false,
+      id: i,
+    }));
 
-  const Tiles = Array(8*8).fill(0).map((_, i) => ({
-    checked: false
-  }));
-
-  /* Ejemplo de linea*/
-  Tiles.map((tile, i) => { if ([12,20,28,36].indexOf(i) >= 0) { tile.checked = true } });
-  
   return (
     <div className="Home">
       <Matriz tiles={Tiles} />
+      <div className="containerBtns">
+        <LeftArrow tiles={Tiles} />
+        <Trash tiles={Tiles} />
+        <RightArrow tiles={Tiles} />
+      </div>
     </div>
   );
 };
